d715ed8f504f543af1a41821b7a85678bfbc0ee8
Hezi cohen <76787135+codeRidersX@users.noreply.github.com>
2022-04-09 17:34:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomDiceImage = "dice" + randomNumber1 + ".png";
-var randomImageSource = "images/" + randomDiceImage;
+
+function shuffleButtons(){
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; // random 1 - 6
+var randomDiceImage = "dice" + randomNumber1 + ".png"; // random dice.png 1-6
+var randomImageSource = "images/" + randomDiceImage; // source for folder // images/dice1.png - images/dice6.png
 var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png"
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSource2);
 
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+
 
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "שחקן 1 ניצח";
@@ -18,3 +25,5 @@ if (randomNumber1 > randomNumber2) {
 } else {
   document.querySelector("h1").innerHTML = "תיקו";
 }
+}
+
